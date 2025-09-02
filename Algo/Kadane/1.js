7464c9a82bf0dd9f57eee49ce596ad633f6aaa6a
Peter Sharma <peterksharma@gmail.com>
2025-09-02 13:45:10 -0500

    started leet code 13

---
+++ Algo/Kadane/1.js
@@ -0,0 +1,17 @@
+let arr=[-2,1,-3,4,-1,2,1,-5,4]
+
+function kadanesAlgorithm(arr) {
+    if (arr.length === 0) return 0;
+    let maxSoFar = arr[0];     
+    let maxEndingHere = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+    console.log(maxSoFar)
+    return maxSoFar;
+}
+
+
+kadanesAlgorithm(arr)
+
