4877593a081756564554807f15664a8f92467b92
Peter Sharma <peterksharma@gmail.com>
2025-09-08 14:00:56 -0500

    solved the cafeteria question

--- Algo/Facebook/Cafeteria.js
+++ Algo/Facebook/Cafeteria.js
@@ -1,11 +1,3 @@
-
-// N = row of seats (count)
-// K = required Space
-// M = Number of Diners
-// S = Position of where theyn are seated
-
-
-
 /**
  * @param {number} N
  * @param {number} K
@@ -13,25 +5,43 @@
  * @param {number[]} S
  * @return {number}
  */
-function getMaxAdditionalDinersCount(N, K, M, S) {
-    K = 2
-    S = [11, 6, 14]
-    
-    let SortedSeats = S.sort((a,b) => a - b)
 
-    let counter = 0
+function getMaxAdditionalDinersCount(N, K, M, S) {
+
+    N = 10
+    K = 1
+    M = 2
+    S = [2, 6]
+
+    let SortedSeats = S.sort((a, b) => a - b);
+    let counter = 0;
     
-    if(SortedSeats[0] > K) {
-        let FirstSeats = Math.floor(SortedSeats[0]/K)
-        for (let i = 0; i < FirstSeats; i++) {
-            counter++
+    //Space Before
+    let spaceBefore = SortedSeats[0] - 1
+    counter += Math.floor((spaceBefore) / (K+1))
+    console.log(counter, 'people fit before the first already sitting person.')
+
+    
+    for (let i = 1; i < SortedSeats.length; i++) {
+        let spaceBetween = ((SortedSeats[i]-1) - SortedSeats[i-1]);
+        let adjustedSpace = spaceBetween - K
+        let peopleThatFitBetween = Math.floor(adjustedSpace/(K+1))
+        if(peopleThatFitBetween > 0 ) {
+            
+            console.log('There ', peopleThatFitBetween, 'people(s) that can fit between seat', SortedSeats[i-1], 'and seat', SortedSeats[i])
+            counter += peopleThatFitBetween
+            console.log('Counter is at:', counter)
         }
     }
-
-    let secondDistace = SortedSeats[1]-SortedSeats[0]
     
+    let spaceAfterLast = N - SortedSeats[SortedSeats.length - 1 ];
+    let spaceAFterCanFit = Math.floor((spaceAfterLast) / (K+1))
+    console.log('This many people fit after the last seated person: ', spaceAFterCanFit)
+    counter += spaceAFterCanFit
+    console.log('counter count after space after:', counter)
 
+    
     return counter;
 }
 
-console.log(getMaxAdditionalDinersCount())
+getMaxAdditionalDinersCount()
---
+++ Algo/Facebook/directorOfPhoto.js
@@ -0,0 +1,33 @@
+// find the first B or P
+// find the the next A
+// find the inverse of what was found in B or P
+// Make sure that its in the contrains of x and y
+
+
+/**
+ * @param {number} N
+ * @param {string} C
+ * @param {number} X
+ * @param {number} Y
+ * @return {number}
+ */
+function getArtisticPhotographCount(N, C, X, Y) {
+    
+    N = 5       //Cell Length
+    C = "APABA"   //Order of things
+    X = 1       //Valid Min Distance
+    Y = 2       //Valid Max Distance
+
+    let counter = 0
+
+    let filledCells = C.split('')
+    console.log(filledCells)
+
+    
+
+
+
+  return counter;
+}
+
+getArtisticPhotographCount()
