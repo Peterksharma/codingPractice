96a3696c02f82aa63d14cd7e7b0ddf76a547570f
Peter Sharma <peterksharma@gmail.com>
2025-09-11 20:43:54 -0500

    working 2460

--- Algo/LeetCode/2460.js
+++ Algo/LeetCode/2460.js
@@ -6,17 +6,31 @@ var applyOperations = function (nums) {
   nums = [1, 2, 2, 1, 1, 0]
 
 
-  for (let i = 0; i < nums.length-1; i++) {
+    for (let i = 0; i < nums.length-1; i++) {
 
     if (nums[i] == nums[i + 1]) {
       nums[i] = nums[i] * 2
-
       nums[i+1] = 0
 
-
     }
-    console.log('i is:', i, 'and the array is', nums)
-  }
+      }
+
+
+    indexDigit = 0 
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== 0) {
+        nums[indexDigit] = nums[i]
+        indexDigit ++
+      }
+    }
+
+    while (indexDigit < nums.length) {
+      nums[indexDigit] = 0
+      indexDigit ++
+    }
+
+  return nums
 };
 
 applyOperations()
