403ad97ea73956f02b4817e16985243ca96e5491
Peter Sharma <peterksharma@gmail.com>
2025-09-21 10:25:11 -0500

    463.js is working

--- Algo/LeetCode/463.js
+++ Algo/LeetCode/463.js
@@ -8,34 +8,29 @@ var islandPerimeter = function(grid) {
     let perimeter = 0
     let rows = grid.length
     let columns = grid[0].length
-
+    
 
     for (let i = 0; i < rows; i++) {
         for(let j = 0; j< columns; j++){
             if (grid[i][j]===1){
-            console.log(i,j, 'is an island space')
             perimeter += 4
             }
             //This is the top check
-            if(i > 0 && grid[i-1][j] === 1) {
-                console.log('The Island has block on top', grid[i][j])
-                perimeter --
+            if(i>0 && grid[i-1][j] === 1 && grid[i][j] === 1 ) {
+                perimeter -= 2
             }
             //Check the left block
-            if(j>0 && grid[i][j-1] === 1) {
-                console.log('Touching to the left')
-                perimeter --
+            if(j>0 && grid[i][j-1] === 1 && grid[i][j] === 1 ) {
+                perimeter -= 2
             }
 
             //Check the right block
-            if(j< columns -1 && grid[i][j+1] === 1) {
-                console.log('Touching to the right')
-                perimeter --
+            if(j > columns -1 && grid[i][j+1] === 1) {
+                perimeter -= 2
             }
         }
         }
         
-console.log(perimeter)
 
 
 }
